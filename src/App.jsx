4a365ec9ebf70dee118b6f9e57fd6dd81192ca6a
Lav Kozakijevic <lavkozakijevic@Lavs-MacBook-Air-2.local>
2025-08-11 16:32:16 +0200

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
-import Pages from "@/pages/index.jsx"
-import { Toaster } from "@/components/ui/toaster"
+import Pages from '@/pages/index.jsx'
+import { Toaster } from '@/components/ui/toaster'
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename="/ad-revenue-calculator">
       <Pages />
       <Toaster />
-    </>
+    </BrowserRouter>
   )
 }
 
-export default App 
+export default App
